397b15b62e7b35b82b431ce4ae730f3c7a4686f6
Qubue <j.wojcicki@yahoo.com>
2017-01-31 14:51:09 +0100

    Formatting form using bootstrap and angularJS

--- AngularForms/AngularForms/app/EmployeeForm/efController.js
+++ AngularForms/AngularForms/app/EmployeeForm/efController.js
@@ -1,4 +1,16 @@
 ﻿angularFormsApp.controller('efController',
     function EmployeeFormController($scope, efService) {
+
         $scope.employee = efService.employee;
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finance",
+            "Administration"
+        ];
+
+        $scope.submitForm(){
+
+        };
     });
